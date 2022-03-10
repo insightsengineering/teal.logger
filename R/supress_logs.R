ecb03384628e2b92a04d76cdb337f894d7730e21
kpagacz <konrad.pagacz@gmail.com>
2022-03-10 12:24:21 +0100

    feat: exported suppress_logs

--- R/supress_logs.R
+++ R/supress_logs.R
@@ -1,11 +1,18 @@
 #' Suppress logger logs
 #'
-#' This function will suppress the logger when running tests
-#' in order to suppress logs for a single test add this function
-#' call within the test_that function. To suppress logs for an entire
-#' test file, call this function at the start of the file
+#' This function suppresses `logger` when running tests via `testthat`.
+#' To suppress logs for a single test, add this function
+#' call within the `testthat::test_that` expression. To suppress logs for an entire
+#' test file, call this function at the start of the file.
+#'
 #' @return `NULL` invisible
-#' @keywords internal
+#' @export
+#' @examples
+#' testthat::test_that("An example test", {
+#'   suppress_logs()
+#'   testthat::expect_true(TRUE)
+#' })
+#'
 suppress_logs <- function() {
   old_log_appenders <- lapply(logger::log_namespaces(), function(ns) logger::log_appender(namespace = ns))
   old_log_namespaces <- logger::log_namespaces()
