7308988e36b34d2adfc7634f310b97da585addd6
m7pr <marcin.kosinski.mk1@roche.com>
2025-11-06 10:41:45 +0100

    apply @gogonzo feedback

--- tests/testthat/test-log_shiny_input_changes.R
+++ tests/testthat/test-log_shiny_input_changes.R
@@ -5,30 +5,29 @@ testthat::describe("log_shiny_input_changes", {
       regexp = "inherits.*reactivevalues"
     )
 
-    mock_session <- shiny::MockShinySession$new()
     shiny::withReactiveDomain(
-      domain = mock_session,
+      domain = shiny::MockShinySession$new(),
       {
         input <- shiny::reactiveValues(x = 1)
 
         testthat::expect_error(
-          log_shiny_input_changes(input = input, namespace = c("a", "b"), session = mock_session),
+          log_shiny_input_changes(input = input, namespace = c("a", "b")),
           regexp = "is.character.*length"
         )
         testthat::expect_error(
-          log_shiny_input_changes(input = input, namespace = 123, session = mock_session),
+          log_shiny_input_changes(input = input, namespace = 123),
           regexp = "is.character"
         )
         testthat::expect_error(
-          log_shiny_input_changes(input = input, excluded_inputs = 123, session = mock_session),
+          log_shiny_input_changes(input = input, excluded_inputs = 123),
           regexp = "is.character"
         )
         testthat::expect_error(
-          log_shiny_input_changes(input = input, excluded_pattern = c("a", "b"), session = mock_session),
+          log_shiny_input_changes(input = input, excluded_pattern = c("a", "b")),
           regexp = "is.character.*length"
         )
         testthat::expect_error(
-          log_shiny_input_changes(input = input, excluded_pattern = 123, session = mock_session),
+          log_shiny_input_changes(input = input, excluded_pattern = 123),
           regexp = "is.character"
         )
         testthat::expect_error(
@@ -39,52 +38,63 @@ testthat::describe("log_shiny_input_changes", {
     )
   })
 
-  it("returns early when log level is too low", {
-    shiny::withReactiveDomain(
-      domain = shiny::MockShinySession$new(),
-      {
-        input <- shiny::reactiveValues(x = 1)
-
-        withr::with_options(list(teal.log_level = "ERROR"), {
-          testthat::expect_null(log_shiny_input_changes(input = input))
-        })
-
-        withr::with_options(list(teal.log_level = logger::ERROR), {
-          testthat::expect_null(log_shiny_input_changes(input = input))
-        })
-
-        withr::with_envvar(list(TEAL.LOG_LEVEL = "ERROR"), {
-          testthat::expect_null(log_shiny_input_changes(input = input))
-        })
-
-        withr::with_options(list(teal.log_level = "INFO"), {
-          testthat::expect_null(log_shiny_input_changes(input = input))
-        })
-      }
-    )
-  })
-
-  it("sets up reactive observers with TRACE level", {
+  it("displays log in the shiny-module when its input changes and teal.log_level <= TRACE", {
     shiny::withReactiveDomain(
       domain = shiny::MockShinySession$new(),
       {
         input <- shiny::reactiveValues(x = 1, y = 2)
-        log_called <- FALSE
+        logged_messages <- character()
 
         testthat::with_mocked_bindings(
-          log_trace = function(...) {
-            log_called <<- TRUE
+          log_trace = function(msg, namespace = NA_character_, ...) {
+            logged_messages <<- c(logged_messages, msg)
             invisible(NULL)
           },
           .package = "logger",
           code = {
             withr::with_options(list(teal.log_level = "TRACE"), {
               log_shiny_input_changes(input = input)
+              # Simulate input change
+              input$x <- 10
+              input$y <- 20
+              # Trigger reactive flush using internal function
+              shiny:::flushReact()
             })
           }
         )
 
-        testthat::expect_true(log_called || is.null(log_shiny_input_changes(input = input)))
+        testthat::expect_true(length(logged_messages) > 0)
+        testthat::expect_true(any(grepl("Shiny input change", logged_messages)))
+      }
+    )
+  })
+
+  it("doesn't display log in shiny-module when its input changes and teal.log_level > TRACE", {
+    shiny::withReactiveDomain(
+      domain = shiny::MockShinySession$new(),
+      {
+        input <- shiny::reactiveValues(x = 1, y = 2)
+        logged_messages <- character()
+
+        testthat::with_mocked_bindings(
+          log_trace = function(msg, namespace = NA_character_, ...) {
+            logged_messages <<- c(logged_messages, msg)
+            invisible(NULL)
+          },
+          .package = "logger",
+          code = {
+            withr::with_options(list(teal.log_level = "INFO"), {
+              log_shiny_input_changes(input = input)
+              # Simulate input change
+              input$x <- 10
+              input$y <- 20
+              # Trigger reactive flush using internal function
+              shiny:::flushReact()
+            })
+          }
+        )
+
+        testthat::expect_length(logged_messages, 0)
       }
     )
   })
